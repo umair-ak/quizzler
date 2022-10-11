6f31c45387a06f0e0ab4ee3875a584f865488f73
Umair <uak1911@gmail.com>
2022-10-12 00:57:31 +0530

    added the UI class in the UI.py and used it in the main.py to create the UI

--- UI.py
+++ UI.py
@@ -1,1 +1,7 @@
-
+from tkinter import* 
+class UI:
+    def __init__(self):
+        self.screen = Tk()
+        self.screen.title("Quizler")
+        self.screen.minsize(width=450, height=600)
+        self.screen.mainloop()
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import data
+import UI as u
 dt = data.Data()
 ques = dt.getQuestions()
+scr = u.UI()
